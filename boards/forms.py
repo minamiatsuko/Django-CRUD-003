3c0469711a933b16a484581e71ca8294224ba086
minamiatsuko <minmin-373@hotmail.co.jp>
2020-06-14 22:16:52 +0200

    フォームAPIを使用してフォーム作成

--- boards/forms.py
+++ boards/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Topic
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    message = forms.CharField(widget=forms.Textarea(), max_length=4000, help_text='The max length of the text is 4000.')
 
     class Meta:
-        model =Topic
+        model = Topic
         fields = ['subject', 'message']
