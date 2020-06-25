7f748aa42f84cff9af0fcfc2638c584069a23582
minamiatsuko <minmin-373@hotmail.co.jp>
2020-06-25 22:23:30 +0200

    Signupのフォーム作成まで Boostrapの適用はまだ

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boards.apps.BoardsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from boards import views
+from accounts import views as accounts_views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', accounts_views.signup, name='signup'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
